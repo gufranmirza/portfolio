ddd7516674f9bcdc6c93e1d7c23dc981aba38373
Gufran Mirza <gufranmirza1@gmail.com>
2021-11-25 19:29:12 +0530

    updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Gufran Mirza',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'I’m a software engineer specializing in building (and occasionally designing) large scale Distributed systems & Softwares',
+    siteUrl: 'https://gufranmirza.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@_imGufran',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
